7bc715ee5dc6b8f524af72161b00554b3d2d030e
Irfan Shadik Rishad <irfanshadikrishad@gmail.com>
2025-02-14 03:30:17 +0600

    fix: signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     }
     signingConfigs {
         create("release") {
-            storeFile = file("slotify.jks")
+            storeFile = file("app/slotify.jks")
             storePassword = System.getenv("STORE_PASSWORD")
             keyAlias = System.getenv("slotify")
             keyPassword = System.getenv("KEY_PASSWORD")
