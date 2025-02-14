985efe5d3481e7b1ad4d065e2abe2f6c944ed341
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-02-14 17:45:48 +0000

    chore: update version to 1.0.21 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         //noinspection OldTargetApi
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0.20"
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
